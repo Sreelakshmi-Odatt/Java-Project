da22aeabfa4a26222dbe9022968545a6feaa80b1
Sreelakshmi Odatt Venu <144397159+Sreelakshmi-Odatt@users.noreply.github.com>
2023-12-27 17:39:07 -0500

    Update Payme.java

--- src/Payme.java
+++ src/Payme.java
@@ -1,11 +1,7 @@
 /*
  File name: Payme.java
  Author: Sreelakshmi Odatt Venu 
- Student number: 041093516
- Course: CST8284 – OOP
- Assignment: Lab 5
- Date: 10/16/2023
- Professor: Veda Vasavi Erukulla
+
  Description:This is is the Payme Interface .
  */
 
@@ -24,8 +20,7 @@ public interface Payme {
 	 * the getPaymentAmount method
 	 * 
 	 * @return no return since it is a interface method Explain the reason why you
-	 *         had to change the name of the earning method to getPaymentAmount() in
-	 *         each class ? 
+	 *       
 	 *         Since we implement the Payme interface in the Programmer
 	 *         class , we have to implemenet th egetPaymentAmount() method .
 	 * 
